3ee0b699aacfc097753bc6c445e717396a3bfcdb
Chikoom <callforcopy@gmail.com>
2020-06-29 11:10:07 +0300

    AJAX and API started

---
+++ AJAX-Intro-to-APIs/main.js
@@ -0,0 +1,15 @@
+// Exercise 1
+exDivider('1');
+
+
+
+
+
+function exDivider(number) {
+  console.log(" ");
+  console.log(" ");
+  console.log("==========================");
+  console.log("Exercise " + number);
+  console.log("==========================");
+  console.log(" ");
+}
